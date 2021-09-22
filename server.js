a3c7bf77ac677b9bc6b7c7081a433e5154af11c7
Dylan K <drkunkel7@gmail.com>
2021-09-22 14:12:40 -0500

    added server.js functionality

---
+++ server.js
@@ -0,0 +1,23 @@
+const mongoose = require(`mongoose`);
+const express = require(`express`);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.user(express.urlencoded({ extended: true }));
+// I don't think this is necessary for just a backend, but it's here now!
+app.user(express.static(`public`));
+
+app.user(require(`./routes`));
+
+// Not sure if this is necessary
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/pizza-hunt`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// this is required to log mongo queries that are executed
+mongoose.set(`debug`, true);
+
+app.listen(PORT, () => console.log(`Now running on localhost:${PORT}`));
